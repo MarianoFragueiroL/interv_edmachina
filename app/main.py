943893a3594d35f164c4a4a55260341746754aed
Mariano <m.fragueiro.lazcano@gmail.com>
2024-07-09 15:46:52 -0300

    add cors

--- app/main.py
+++ app/main.py
@@ -4,7 +4,11 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from app.api.api_v1.api import api_router
 from app.db.session import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
+origins = [
+    "http://localhost:3000",
+]
 def init_db():
     Base.metadata.create_all(bind=engine)
 
@@ -22,9 +26,15 @@ def create_app() -> FastAPI:
     app.include_router(api_router, prefix="/api")
 
     return app
-
 app = create_app()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to my FastAPI project!"}
